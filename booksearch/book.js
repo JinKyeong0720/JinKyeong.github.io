b48ceafbbc723503fdc1c2e65baed5d7319706be
JinKyeong0720 <jj9jj98664@naver.com>
2023-03-10 18:31:20 +0900

    Create Project bookSearch

---
+++ booksearch/book.js
@@ -0,0 +1,66 @@
+// API : Application Programming Interface
+// ㄴ Open API : 다양한 회사에서 공익의 목적으로, 또는 다른 이유로 무료로 인터페이스를 이용할 수 있게 제공
+// ㄴ Private API : 유료
+
+// Open API
+// ㄴ 공공 데이터
+// ㄴ 
+// ㄴ 
+// ...
+
+// ajax
+// ㄴ 비동기 방식으로 페이지의 일부 정보를 갱신할 수 있는 기술
+// fetch()로도 구현 가능 (일부 브라우저 또는 하위 버전의 스크립트에서 호환 X)
+// -> jQuery.ajax() 메소드를 활용
+
+let page = 1;
+
+const query = document.querySelector(".query");
+
+
+const searchBox = document.querySelector(".search-box");
+searchBox.addEventListener("submit", e=>{
+    e.preventDefault();         // submit 기본동작
+
+    if (query !== ""){
+        searchRequest(query.value, page);
+    }
+
+    searchRequest(query.value);
+});
+
+
+function searchRequest(query, page){
+    console.log("query : ", query);
+
+    $.ajax({ // 세팅정보 오브젝트로 넘겨줌
+        "url": `https://dapi.kakao.com/v2/search/blog?query=${query}&page=1&size=1&target=title`, // url 키값 스트링타입 : 파라미터까지 구성돼있음
+        "method": "GET", // 요청 메소드 : 어떤 방식(get, toString 등등)
+        "timeout": 0, // 비동기 시간(기본 0)
+        "headers": { // 오브젝트 타입으로 따로 구성했음(key value)
+            "Authorization": "KakaoAK 11a0270989726c0e03b2a8bf83db098e"
+        },
+    })
+    
+    .done((response) => { // 성공했을 때
+        // console.log(response);
+        // container 안에 
+
+        /*
+        <div class="container">
+        <div class="result-card">
+            <img class="book-img" src="/book.png">
+            <h4 class="book-title">도서 제목</h4>
+            <p class="book-description">도서상세정보</p>
+            <span class="price">10000원</span>
+            <p class="book-info">
+                <span class="author">저자</span>|<span class="publisher">출판사</span>
+            </p>
+        </div>
+        */
+
+        // 새로 생성 및 구성 완료한 result-card element(요소)를 추가
+    });
+}
+
+
